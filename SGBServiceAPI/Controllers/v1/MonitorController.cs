ea1e631b8627853f4f281c0470bbf5152258c610
Dimakatso Mashao <DMashao@skhomotech.co.za>
2022-04-19 16:57:27 +0200

    Migration

---
+++ SGBServiceAPI/Controllers/v1/MonitorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SNServiceAPI.Models;
+using SNServiceAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNServiceAPI.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonitorController : ControllerBase
+    {
+        private readonly IDapper _dapper;
+        public MonitorController(IDapper dapper)
+        {
+            _dapper = dapper;
+        }
+        [HttpGet(nameof(DistrictCodeByPosition))]
+        public Task<List<MonitorModel>> DistrictCodeByPosition(string DistrictCode,string Position)
+        {
+            var result = Task.FromResult(_dapper.GetAll<MonitorModel>($" select * from [tblUsers] where DistrictCode LIKE'{DistrictCode}' AND Position LIKE'{Position}'", null,
+                    commandType: CommandType.Text));
+            return result;
+        }
+    }
+}
---
+++ SGBServiceAPI/Models/MonitorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SNServiceAPI.Models
+{
+    public class MonitorModel
+    {
+        public string DistrictCode { get; set; }
+        public string Position { get; set; }
+        public string Region { get; set; }
+        public string DistrictName { get; set; }
+        public string EmisNumber { get; set; }
+    }
+}
